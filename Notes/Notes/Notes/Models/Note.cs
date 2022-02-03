cab2b998933bce8a674524098de2c4a15cb14fa9
EmanueleUltima <98026372+EmanueleUltima@users.noreply.github.com>
2022-02-03 12:04:39 +0100

    Progetto password

---
+++ Notes/Notes/Notes/Models/Note.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using SQLite;
+
+namespace Notes.Models
+{
+    public class Note
+    {
+        [PrimaryKey, AutoIncrement]
+        public int ID { get; set; }
+        public string ServiceName { get; set; }
+        public string Username { get; set; }
+        public string Password { get; set; }
+        public string URL { get; set; }
+        public DateTime Date { get; set; }
+    }
+}
